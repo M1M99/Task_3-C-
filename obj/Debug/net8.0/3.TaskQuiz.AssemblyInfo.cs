33fcdab17cc9c09a7b1cf51a4cf1a7b2bb8d5dc6
ASUS <ASUS@DESKTOP-NB7MT4D>
2024-05-30 21:22:38 +0400

    taskss

--- obj/Debug/net8.0/3.TaskQuiz.AssemblyInfo.cs
+++ obj/Debug/net8.0/3.TaskQuiz.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("3.TaskQuiz")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e927e6611b2cd7da42ce177bb15bf9edd9489def")]
 [assembly: System.Reflection.AssemblyProductAttribute("3.TaskQuiz")]
 [assembly: System.Reflection.AssemblyTitleAttribute("3.TaskQuiz")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
